3aafcb5ef5bce25f8e40cc50e885f7c05cefd417
danny-jin <icarus96119@gmail.com>
2022-07-07 18:33:14 +0900

    remove: EthSpring.sol update: test script

--- test/bsc.ts
+++ test/bsc.ts
@@ -3,30 +3,58 @@ import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
 describe("Seasonal Bridge Bsc test network", () => {
+  const adminAddress = "0xF5EB5549306b4c05B7D40b91500d3eB440c4576a";
   let Token;
-
   let springToken: Contract,
+    summerToken: Contract,
+    autumnToken: Contract,
+    winterToken: Contract,
     bscBridgeContract: Contract,
-    deployer: SignerWithAddress,
-    admin: SignerWithAddress;
+    deployer: SignerWithAddress;
   describe("Deploy", () => {
     it("Should deploy the contracts", async () => {
-      [deployer, admin] = await ethers.getSigners();
+      [deployer] = await ethers.getSigners();
       console.log("deployer: ", deployer.address);
-      console.log("admin: ", admin.address);
+
       Token = await ethers.getContractFactory("Spring");
-      springToken = await Token.deploy(admin.address);
+      springToken = await Token.deploy(adminAddress);
       console.log("springToken address: ", springToken.address);
+
+      Token = await ethers.getContractFactory("Summer");
+      summerToken = await Token.deploy(adminAddress);
+      console.log("summerToken address: ", summerToken.address);
+
+      Token = await ethers.getContractFactory("Autumn");
+      autumnToken = await Token.deploy(adminAddress);
+      console.log("autumnToken address: ", autumnToken.address);
+
+      Token = await ethers.getContractFactory("Winter");
+      winterToken = await Token.deploy(adminAddress);
+      console.log("winterToken address: ", winterToken.address);
+
       console.log(
         "springToken verify: ",
-        `npx hardhat verify --contract "contracts/Spring.sol:Spring" --network bscTestnet ${springToken.address} ${admin.address}`
+        `npx hardhat verify --contract "contracts/Spring.sol:Spring" --network bscTestnet ${springToken.address} ${adminAddress}`
       );
-      Token = await ethers.getContractFactory("BscBridge");
-      bscBridgeContract = await Token.deploy(admin.address);
+      console.log(
+        "summerToken verify: ",
+        `npx hardhat verify --contract "contracts/Summer.sol:Summer" --network bscTestnet ${summerToken.address} ${adminAddress}`
+      );
+      console.log(
+        "autumnToken verify: ",
+        `npx hardhat verify --contract "contracts/Autumn.sol:Autumn" --network bscTestnet ${autumnToken.address} ${adminAddress}`
+      );
+      console.log(
+        "winterToken verify: ",
+        `npx hardhat verify --contract "contracts/Winter.sol:Winter" --network bscTestnet ${winterToken.address} ${adminAddress}`
+      );
+
+      const BscBridgeContract = await ethers.getContractFactory("BscBridge");
+      bscBridgeContract = await BscBridgeContract.deploy(adminAddress);
       console.log("bscBridgeContract address: ", bscBridgeContract.address);
       console.log(
         "bscBridgeContract verify: ",
-        `npx hardhat verify --contract "contracts/BscBridge.sol:BscBridge" --network bscTestnet ${bscBridgeContract.address} ${admin.address}`
+        `npx hardhat verify --contract "contracts/BscBridge.sol:BscBridge" --network bscTestnet ${bscBridgeContract.address} ${adminAddress}`
       );
     });
   });
--- test/eth.ts
+++ test/eth.ts
@@ -3,65 +3,21 @@ import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
 describe("Seasonal Bridge Test eth network", () => {
+  const adminAddress = "0xF5EB5549306b4c05B7D40b91500d3eB440c4576a";
   let Token;
-
-  let springToken: Contract,
-    ethBridgeContract: Contract,
-    deployer: SignerWithAddress,
-    admin: SignerWithAddress;
+  let ethBridgeContract: Contract, deployer: SignerWithAddress;
   describe("Deploy", () => {
     it("Should deploy the contracts", async () => {
-      [deployer, admin] = await ethers.getSigners();
+      [deployer] = await ethers.getSigners();
       console.log("deployer: ", deployer.address);
-      console.log("admin: ", admin.address);
-      Token = await ethers.getContractFactory("Spring");
-      springToken = await Token.deploy(admin.address);
-      console.log("springToken address: ", springToken.address);
-      console.log(
-        "springToken verify: ",
-        `npx hardhat verify --contract "contracts/Spring.sol:Spring" --network rinkeby ${springToken.address} ${admin.address}`
-      );
+      console.log("admin: ", adminAddress);
       Token = await ethers.getContractFactory("EthBridge");
-      ethBridgeContract = await Token.deploy(admin.address);
+      ethBridgeContract = await Token.deploy(adminAddress);
       console.log("ethBridgeContract address: ", ethBridgeContract.address);
       console.log(
         "ethBridgeContract verify: ",
-        `npx hardhat verify --contract "contracts/EthBridge.sol:EthBridge" --network rinkeby ${ethBridgeContract.address} ${admin.address}`
+        `npx hardhat verify --contract "contracts/EthBridge.sol:EthBridge" --network rinkeby ${ethBridgeContract.address} ${adminAddress}`
       );
-    });
-  });
-
-  describe("Spring Token Mint", () => {
-    it("Should mint tokens between accounts", async () => {
-      let tx = await springToken
-        .connect(admin)
-        .mint(deployer.address, "100000000000000000000000000000000000000000");
-      await tx.wait();
-      tx = await springToken
-        .connect(admin)
-        .mint(admin.address, "100000000000000000000000000000000000000000");
-      await tx.wait();
-    });
-  });
-
-  describe("Approve Spring Token to EthBridge", () => {
-    it("Should approve spring token to ethBridge", async () => {
-      const tx = await springToken
-        .connect(deployer)
-        .approve(
-          ethBridgeContract.address,
-          "100000000000000000000000000000000000000000"
-        );
-      await tx.wait();
-    });
-  });
-
-  describe("Swap Spring Token from Eth", () => {
-    it("Should swap spring token from eth", async () => {
-      const tx = await ethBridgeContract
-        .connect(deployer)
-        .swapFromEth(springToken.address, "1000000000000000000");
-      await tx.wait();
     });
   });
 });
